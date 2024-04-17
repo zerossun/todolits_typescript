5ba17e91a75e46fc1ce316e38cdff4f317a2a375
zerossun <wonsunyoung0703@gmail.com>
2024-04-17 17:59:34 +0900

    curd

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import PokemIndex from './components/PokemIndex'
 import PokemonDetailsPage from './components/PokemonDetailsPage';
+import Create from './components/Create'
+import UseEffect from './components/UseEffect'
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import './App.css';
 
@@ -8,8 +10,10 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<PokemIndex />} />
-        <Route path='/pokemon/:id' element={<PokemonDetailsPage/>} />
+        {/* <Route path='/' element={<PokemIndex />} /> */}
+        {/* <Route path='/' element={<UseEffect />} /> */}
+        {/* <Route path='/pokemon/:id' element={<PokemonDetailsPage/>} /> */}
+        <Route path='/' element={<Create/>}></Route>
       </Routes>
     </Router>
     
